9e307ecca17bbfd3d9e54fc1b1ca9305c1871a5c
Jacob Pattison <18pattisonj@utcsheffield.org.uk>
2024-07-03 15:11:02 +0100

    Level Scrolling Clamping at bottom too much

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -7,23 +7,11 @@ public class LevelSelector : MonoBehaviour
     [SerializeField] private GameObject LevelPrefab;
     [SerializeField] private int LevelCount;
     [SerializeField] private List<GameObject> LevelList;
-    [SerializeField] private Transform LevelsTransform;
+    [SerializeField] private RectTransform LevelsTransform;
     void Start()
     {
-        int counter = 0;
-        int x = -270;
-        int y = 80;
-        while (counter <= this.LevelCount + 1)
-        {
-            if (x > 270) x = -270;
-            GameObject temp = Instantiate(LevelPrefab, new Vector2(x, y), Quaternion.identity, LevelsTransform);
-            temp.transform.position = new Vector3(x, y, 0);
-            LevelList.Add(temp);
-            counter++;
-            x += 180;
-            if (counter % 4 == 0) y += 80;
-        }
+        
     }
 
-   
+
 }
