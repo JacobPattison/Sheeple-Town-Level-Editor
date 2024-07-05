3aaf8ccabe025b22abe90498843b11f2c835dd2e
Jacob Pattison <18pattisonj@utcsheffield.org.uk>
2024-07-05 15:43:05 +0100

    levels are got from folder

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public string Path;
+    public string LevelName;
+    public TMPro.TMP_Text Text;
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Linq;
 
 public class LevelSelector : MonoBehaviour
 {
     [SerializeField] private GameObject LevelPrefab;
-    [SerializeField] private int LevelCount;
     [SerializeField] private List<GameObject> LevelList;
     [SerializeField] private RectTransform LevelsTransform;
+
+    private string LevelDirectory = Application.dataPath + "/Levels";
+
     void Start()
     {
-        
+        string[] allPaths = Directory.GetFiles(LevelDirectory); // Gets all files, including meta files
+        string[] levelPaths = allPaths.Where(s => s.EndsWith(".txt")).ToArray(); // Takes only the .txt files from all files
+
+        int levelIndex = 0;
+        foreach (string levelPath in levelPaths)
+        {
+            string filePath = Path.GetFileName(levelPath); // Gets current file path, with .txt at the end
+            string levelName = filePath.Substring(0, filePath.Length - 4); // Gets only the name of the level from path
+
+            LevelList.Add(Instantiate(LevelPrefab, LevelsTransform));
+            LevelList[levelIndex].GetComponent<Level>().Path = levelPath;
+
+            LevelList[levelIndex].GetComponent<Level>().LevelName = levelName;
+            LevelList[levelIndex].GetComponent<Level>().Text.text = levelName;
+
+            levelIndex++;
+        }
     }
 
 
