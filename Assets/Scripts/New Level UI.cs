facc2796a5a11fd0452e988222c69092420b9b01
Jacob Pattison <jpattison129@gmail.com>
2024-09-19 12:08:11 +0100

    Level Validation Done

--- Assets/Scripts/New Level UI.cs
+++ Assets/Scripts/New Level UI.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class NewLevelUI : MonoBehaviour
 {
     [SerializeField] TMP_InputField NameInputField;
     [SerializeField] TMP_InputField WidthInputField;
     [SerializeField] TMP_InputField HeightInputField;
+    [SerializeField] TMPro.TMP_Text Error;
 
     private string LevelDirectory = Application.dataPath + "/Levels/";
 
@@ -17,6 +19,27 @@ public class NewLevelUI : MonoBehaviour
         string levelName = NameInputField.text;
         string levelWidth = WidthInputField.text;
         string levelHeight = HeightInputField.text;
+
+        if (levelName == "" || levelWidth == "" || levelHeight == "")
+        {
+            Error.text = "All Input Fields Must Be Filled";
+            return;
+        }
+        if (levelName.Length > 100)
+        {
+            Error.text = "Level Name Can Not Be Greater Than 100 Character";
+            return;
+        }
+        if (!levelHeight.All(char.IsDigit) || !levelWidth.All(char.IsDigit))
+        {
+            Error.text = "Level Dimentions Should Only Contain Numbers";
+            return;
+        }
+        if (int.Parse(levelHeight) > 100 || int.Parse(levelWidth) > 100)
+        {
+            Error.text = "Level Dimentions Should Be Less Than 100 In Size";
+            return;
+        }
 
         string path = LevelDirectory + levelName + ".txt";
 
