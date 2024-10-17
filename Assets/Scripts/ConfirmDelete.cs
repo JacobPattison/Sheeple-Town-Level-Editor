1b87d6ca811ce8c45c31794b2e1c79463fa0e396
Jacob Pattison <jpattison129@gmail.com>
2024-10-17 10:22:24 +0100

    .

--- Assets/Scripts/ConfirmDelete.cs
+++ Assets/Scripts/ConfirmDelete.cs
@@ -8,7 +8,7 @@ public class ConfirmDelete : MonoBehaviour
     public GameObject level;
     public string Path;
 
-
+    
     public void ConfirmDeleteLevel()
     {
         Debug.Log("Deleted Level: " + this.Path);
